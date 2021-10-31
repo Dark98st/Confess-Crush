c94276d7036b32389a2d181822ea86d2669a26b7
Dark98st <89790659+Dark98st@users.noreply.github.com>
2021-10-31 15:55:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,20 @@
 const CONFIG = {
-    titleWeb: "Thi thật tốt nhe",
-    introTitle: 'Bạn đặc biệt của tui 🥰',
-    introDesc: `Wishing you luck and success in your recent exam!`,
+    titleWeb: "Tiêu đề web",
+    introTitle: 'Tên người yêu bạn',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Bí quyest để thi thật tốt🥰',
-    desc: 'Phải thật bình tĩnh, tự tin, à nhớ ôn bài nữa.. Đặc biệt là đọc được cái nàyyy ',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Thi xong thì liệu bạn có suy nghĩ thêm về chuyện tìm hiểu tuii hemmmmm',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Cái này thì chắc để từ từ... Xin lỗi bạn nha',
-    mess: 'Mình biết mà 🥰. Không saoooo tui chờ được 😘😘',
-    messDesc: 'Đọc xong thì chuẩn bị đi ngủ sớm  để mai thật sự tỉnh táo nha.',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-  }
+    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
