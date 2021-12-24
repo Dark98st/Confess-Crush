0f8848e35631f79c9e27bff9847f5ec4269b5e53
Dark98st <89790659+Dark98st@users.noreply.github.com>
2021-12-24 11:38:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Giáng sinh an lành",
+    introTitle: '"EM BỒ" BẢO LINH',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnIntro: 'HIHIHI',
+    title: 'Chúc em một đêm giáng sinh thật an lành vui vẻ và hạnh phúc nhoa🥰',
+    desc: 'Anh khum biết tặng em gì nên đành edit này tặng em thui hà, Khum biết em thích khum nữa',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên APP có hơn hàng trăm người vậy mà anh vs em match được và nói chuyện với nhau cũng được 3 tuần r ha..Anh Thích lắm <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    reply: 'Em cũng thích ..Mong đến ngày gặp nhau ghê 😘😘',
+    mess: 'Anh cũng mong lắm nà 🥰. Thương em nhiều nhiều 😘😘',
+    messDesc: 'Đêm giáng sinh năm nay khum còn lạnh lẽo nữa r ha.',
+    btnAccept: 'Đúng r nà <3',
+    messLink: 'https://www.facebook.com/kevodung.nhat/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
